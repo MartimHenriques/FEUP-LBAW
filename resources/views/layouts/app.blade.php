3bd3fa2a0eb0688a02e538c8978300c27df6e188
up202007191 <up202007191@fe.up.pt>
2022-11-23 17:08:14 +0000

    css fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <main>
       <header>
-        <a href="{{ url('/') }}"><img src="/../logo.png" alt="logo"></a>
+        <a href="{{ url('/') }}"><img id="logo" src="/../logo.png" alt="logo"></a>
         <div><input type="text" placeholder="search"></div>
         <!--<a class="button" href="{{ route('eventsCreate') }}"> Create event </a>--><!--TODO-->
         @if (Auth::check())
