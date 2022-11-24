5fd1e139ab6e9b0ac192a4356330c1e3ea45923e
up202004229 <up202004229@fe.up.pt>
2022-11-24 21:34:42 +0000

    updating a7

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -10,7 +10,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
-
 class SearchController extends Controller
 {
     public function search(Request $request) {
