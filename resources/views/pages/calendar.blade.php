da566733c71497e8bc39652e789ff0113a563f8c
up202004229 <up202004229@fe.up.pt>
2022-11-24 23:55:22 +0000

    BRUNA

--- resources/views/pages/calendar.blade.php
+++ resources/views/pages/calendar.blade.php
@@ -4,9 +4,21 @@
 
 @section('content')
 
-<h1>Events attended/to attend</h1>
-<div class="myevents">
-    @each('partials.eventCard', $myevents, 'event')
+<h1>Events attended</h1>
+<div class="eventsAttended">
+    @if(count($eventsattended)>0)
+    @each('partials.eventCard', $eventsattended, 'event')
+    @else
+    <p>You haven't been to any event yet</p>
+    @endif
     
+</div>
+<h1>Events to attend</h1>
+<div class="eventsToAttend">
+    @if(count($eventstoattend)>0)
+    @each('partials.eventCard', $eventstoattend, 'event')
+    @else
+    <p>You don't have events to attend</p>
+    @endif
 </div>
 @endsection
